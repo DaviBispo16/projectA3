b0ea674df779477978f1d1b2dd690f873f420694
Davi Bispo Pereira Cruz <davibispo2004@hotmail.com>
2024-05-16 21:47:29 -0300

    feat: employee register created

--- app/src/application/Program.java
+++ app/src/application/Program.java
@@ -16,16 +16,42 @@ public class Program {
 		List <Employee> employees = new ArrayList<>();
 		
 		System.out.println("-----Controle de funcionários-----");
-		System.out.println("Menu");
+
 		int userChoice;
 	
 		do {
+			System.out.println("MENU");
 			System.out.println("[1] Cadastrar Funcionário");
 			System.out.println("[2] Listar Funcionários");
 			System.out.println("[3] Sair");
 			
 			userChoice = sc.nextInt();
 			
+			if (userChoice == 1) {
+				 System.out.println();
+				 System.out.println("-----Cadastro-----");
+				 
+				 System.out.print("Nome:");
+				 sc.nextLine(); 
+				 String name = sc.nextLine();
+				 
+				 System.out.print("Email:");
+				 String email = sc.next();
+				 
+				 System.out.print("CPF:");
+				 String cpf = sc.next();
+				 
+				 System.out.print("Tipo de Contrato:");
+				 sc.nextLine();
+				 String employmentContract = sc.nextLine();
+				 
+				 System.out.print("Salário Bruto:");
+				 Double grassSalary = sc.nextDouble();
+				 
+				 employees.add(new Employee(name, email, cpf, employmentContract, grassSalary));
+				 System.out.println("Funcionário cadastrado com sucesso!");
+				 System.out.println();
+			}
 			
 			
 		} while (userChoice == 1 || userChoice == 2);
